93ccb17472d63572e6f4fc5b2b4b3da363184c17
dubidub <dub.wu.dub@gmail.com>
2022-08-21 15:55:54 +0800

    hotel perk finder pointimize 1.1.1
    add pointimize logo
    add GA

---
+++ hotel-perks-finder/resources/perks_collection_desc.js
@@ -0,0 +1,21 @@
+var perks_desc = {
+  'CBF': 'Complementary Breakfast',
+  'CRU': 'Room Upgrades',
+  'ECI': 'Early Check-in',
+  'LCO': 'Late Check-out',
+  'FCD': 'Free Credit',
+  'IRG': 'In-room Gift',
+  'WGA': 'Welcome Gift',
+  'CIN': 'Complementary Internet',
+  'OTH': 'Others',
+};
+
+var elite_desc = {
+  'EP00FH': 'AMEX FINE HOTELS+RESORTS',
+  'EP00HC': 'AMEX Hotel Collection',
+  'EP00KC': 'Kiwi Collection',
+  'EP00TP': 'Tablet Hotels',
+  'EP00VL': 'Visa Luxury Hotel Collection',
+  'EP00VS': 'Visa Signature',
+  'EP00VT': 'Virtuoso'
+}
